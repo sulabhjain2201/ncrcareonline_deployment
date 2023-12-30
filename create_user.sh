93e3132a43e9701b87e74e5c7597193d61c9857a
jainsulabh1991 <jainsulabh1991@gmail.com>
2023-12-30 21:44:44 +0530

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+
+set -x #echo on
+
+
+export displayName=$1
+
+export userPrincipalName=$2
+
+export passwordProfile=$3
+
+
+
+az ad user create \
+  --display-name "$displayName" \
+  --user-principal-name "$userPrincipalName" \
+  --password "$passwordProfile" \
+  --force-change-password-next-login true
+
+# Output success message
+echo "User created successfully."
+
 
